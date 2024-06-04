f91030b8ba70ea4159897e052efa98c57ec3368d
22jaimee <jmoragues07@gmail.com>
2024-06-04 13:29:29 +0200

    Añade Javadoc

--- PracticaMaximosYMinimos/src/clases/Minimos.java
+++ PracticaMaximosYMinimos/src/clases/Minimos.java
@@ -5,7 +5,12 @@ import java.util.Collections;
 
 public abstract class Minimos {
 
-	
+	/**
+	 * Metodo para devorlver el valor maximos de 2 valores
+	 * @param a
+	 * @param b
+	 * @return el valor maximo entre 2 valores
+	 */
 	
 	public static float MinimoDe2EnterosPositivos(int a, int b) {
 		if(a<0 || b<0) {
@@ -18,6 +23,14 @@ public abstract class Minimos {
 			return b;
 		}
     }
+	
+	/**
+	 * Metodo para devorlver el valor maximos de 3 valores
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return devuelve el valor maximo entre 3 valores
+	 */
 	
 	public static float MinimoDe3EnterosPositivos(int a, int b, int c) {
 		if(a<0 || b<0 || c<0) {
